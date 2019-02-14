2e7f2c32ec844a5e5550509f517d3682a9731848
Liam D <elmdecoste@gmail.com>
2019-02-14 11:34:25 -0600

    Initial Commit

---
+++ MVVMEnforcer/MVVMEnforcer.h
@@ -0,0 +1,19 @@
+//
+//  MVVMEnforcer.h
+//  MVVMEnforcer
+//
+//  Created by Liam DeCoste on 2/14/19.
+//  Copyright © 2019 elmd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MVVMEnforcer.
+FOUNDATION_EXPORT double MVVMEnforcerVersionNumber;
+
+//! Project version string for MVVMEnforcer.
+FOUNDATION_EXPORT const unsigned char MVVMEnforcerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MVVMEnforcer/PublicHeader.h>
+
+
